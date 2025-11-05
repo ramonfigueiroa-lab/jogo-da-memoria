8ce91b5e7ea8e667e3c603d5de6870c2f983f006
ramonfigueiroa-lab <ramonfigueiroa@gmail.com>
2025-11-04 22:55:52 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,23 @@
-const CACHE_NAME = "memoria-cache-v1";
-const FILES_TO_CACHE = [
-  "./memoria.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-// Instala o service worker e armazena os arquivos no cache
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open("memoria-cache-v1").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
     })
   );
-  self.skipWaiting();
 });
 
-// Ativa o service worker e remove caches antigos
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Intercepta requisições e serve do cache quando possível
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
 });
+
 
