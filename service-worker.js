6d87547ece8e19709a10432438accc8fc67fa6e6
ramonfigueiroa-lab <ramonfigueiroa@gmail.com>
2025-11-04 23:20:56 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,28 @@
-const CACHE_NAME = 'jogo-memoria-v1';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE_NAME = "memoria-cache-v1";
+const urlsToCache = [
+  "/jogo-da-memoria/",
+  "/jogo-da-memoria/index.html",
+  "/jogo-da-memoria/style.css",
+  "/jogo-da-memoria/script.js",
+  "/jogo-da-memoria/manifest.json",
+  "https://i.imgur.com/xDZQ5dH.jpg",
+  "https://i.imgur.com/9Pet8B3.jpg",
+  "https://i.imgur.com/PDUWW9q.jpg",
+  "https://i.imgur.com/pO3bFOn.jpg",
+  "https://i.imgur.com/bwNt2Uv.jpg",
+  "https://i.imgur.com/EYe3bmY.jpg",
+  "https://i.imgur.com/OdzIlpf.jpg",
+  "https://i.imgur.com/Qj50kbC.jpg"
 ];
 
-// Install - cache files
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
-  self.skipWaiting();
 });
 
-// Activate - cleanup old caches
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.map((key) => { if (key !== CACHE_NAME) return caches.delete(key); })
-    ))
-  );
-  self.clients.claim();
-});
-
-// Fetch - respond from cache, otherwise network
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then((resp) => resp || fetch(evt.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
